b7a42270778b227a9ea211f37fd7f306ea1452b3
ahmedeid2260 <a.email2260@gmail.com>
2026-05-17 10:04:49 +0300

    chore(next.config): add translation globally

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  i18n: {
+    locales: ['en', 'ar'],
+    defaultLocale: 'en',
+    // direction: { ar: 'rtl' },
+  },
 };
-
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
