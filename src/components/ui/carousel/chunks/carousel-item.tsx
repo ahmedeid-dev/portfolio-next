da00962474680b0e0a131b0ac7a897d9b51c4c7c
ahmedeid2260 <a.email2260@gmail.com>
2026-05-18 09:47:55 +0300

    feat(carousel-item): add carousel-item component

---
+++ src/components/ui/carousel/chunks/carousel-item.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/lib/utils"
+import { useCarousel } from "./use-carousel"
+
+function CarouselItem({ className, ...props }: React.ComponentProps<"div">) {
+    const { orientation } = useCarousel()
+
+    return (
+        <div
+            role="group"
+            aria-roledescription="slide"
+            data-slot="carousel-item"
+            className={cn(
+                "min-w-0 shrink-0 grow-0 basis-full",
+                orientation === "horizontal" ? "ps-4" : "pt-4",
+                className
+            )}
+            {...props}
+        />
+    )
+}
+
+export { CarouselItem }
